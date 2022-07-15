3605d47ce8bbf36706999813c35c76cd61d7bc7d
Glaxer <Skoleernederen@live.dk>
2022-07-15 18:28:00 +0200

    Added firebase & firestore

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCQoUB-NyX36Ds1MsdhMF-U9lftG3CrmQs",
+    authDomain: "raketligaen.firebaseapp.com",
+    projectId: "raketligaen",
+    storageBucket: "raketligaen.appspot.com",
+    messagingSenderId: "613741254561",
+    appId: "1:613741254561:web:acfd9532cf171f0b8e9de0",
+    measurementId: "G-TZ8T90YXZN"
+}
---
+++ src/firebaseInit.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+import firebaseConfig from './firebaseConfig'
+
+const firebaseapp = firebase.initializeApp(firebaseConfig)
+
+export default firebaseapp.firestore()
