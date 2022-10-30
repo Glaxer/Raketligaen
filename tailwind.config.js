fffbb710c09a12500afe2ee68ab5b0685310beda
Glaxer <Skoleernederen@live.dk>
2022-10-30 13:48:42 +0100

    Tailwind color settings and update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
         "./src/**/*.{vue,js,ts,jsx,tsx}",
     ],
     theme: {
+        colors: {
+            'bg': '#F6F7EB',
+            'dark': '#393E41',
+            'primary': '#A30026',
+            'secondary': '#2C8C99',
+            'orange': '#E94F37',
+        },
+        fontFamily: {
+            oswald: ['Oswald'],
+        },
         extend: {},
     },
     plugins: [],
