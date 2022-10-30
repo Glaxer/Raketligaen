66edd33b8591a1c4efc9c1afbd8c3dbd1466d473
Glaxer <Skoleernederen@live.dk>
2022-10-30 14:56:50 +0100

    Format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,17 @@
 module.exports = {
-    content: [
-        "./index.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
-    ],
-    theme: {
-        colors: {
-            'bg': '#F6F7EB',
-            'dark': '#393E41',
-            'primary': '#A30026',
-            'secondary': '#2C8C99',
-            'orange': '#E94F37',
-        },
-        fontFamily: {
-            oswald: ['Oswald'],
-        },
-        extend: {},
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      bg: '#F6F7EB',
+      dark: '#393E41',
+      primary: '#A30026',
+      secondary: '#2C8C99',
+      orange: '#E94F37',
     },
-    plugins: [],
-}
+    fontFamily: {
+      oswald: ['Oswald'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
